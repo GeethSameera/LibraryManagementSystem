8719fc60ac691b0b41af32cd900b80bd68f2e880
GeethSameera <geethsameera077@gmail.com>
2018-11-23 07:46:36 +0530

    routes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var memberRouter = require('./routes/member');
 
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
 })
+
+app.use('/addmember', memberRouter);
 
 app.listen(port, (err) => {
   if (err) {
